fb02140f783f96d9cf47076d4b0e27d407418f6c
NyaYo <76841616+NyaYoChan@users.noreply.github.com>
2023-01-09 22:00:39 +0800

    Add files via upload
    2023.1.9 Complete Ver 1.1.

---
+++ Doggy_SnakeGame-main/src/snake/Main.java
@@ -0,0 +1,27 @@
+package snake;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("SnakeGame");
+        
+        Image iconImage = new Image(getClass().getResource(Setting.ICON_IMAGE_PATH).toString());
+        primaryStage.getIcons().add(iconImage);
+        
+        primaryStage.setResizable(false);
+        
+        Setting.SCENEMANAGER = new SceneManager(primaryStage);
+        Setting.SCENEMANAGER.buildMenu();
+        
+        primaryStage.show();
+    }
+    
+}
